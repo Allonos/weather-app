2751b437e66881751d4602d8147a823c2657fb5c
Rezy Tsitsvidze <101000533+Allonos@users.noreply.github.com>
2023-01-10 16:27:56 +0400

    Summary
    This project shows every countries(over 1 million)  weather and 7 days forecast.

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '9abed36fbcmsha9f0f78c723101dp1d3751jsnb8153d2b676d',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'bc8e22615ad6a5482ed1ba2883ea00ef';
